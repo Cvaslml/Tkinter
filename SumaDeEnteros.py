e7a6c36e64ef3e945bb871f932bb1bbeef676cd5
Johan Sebastian Herrera Melgarejo <102569088+Cvaslml@users.noreply.github.com>
2022-07-09 12:29:18 -0500

    Add files via upload

---
+++ SumaDeEnteros.py
@@ -0,0 +1,110 @@
+# Importo la libreria tkinter, con el alias tk
+import tkinter as tk
+from tkinter import messagebox
+# ------------------------------
+# Funciones operaciones
+# ------------------------------
+def calcular():
+    messagebox.showinfo("Suma enteros", "Hizo click en el boton Calcular")
+def borrar():
+    messagebox.showinfo("Suma enteros", "Hizo click en el boton Borrar")
+def salir():
+    messagebox.showinfo("Suma enteros", "La app se cerrará")
+    ventana_principal.destroy()
+# ------------------------------
+# Ventana principal
+# ------------------------------
+
+
+# Creacion objeto Tk (ventana principal)
+ventana_principal = tk.Tk()
+
+
+# Titulo ventana_principal
+ventana_principal.title("Suma enteros")
+
+# Tamaño o dimensiones de la ventana
+ventana_principal.geometry("500x500")
+
+# Deshabilitamos el boton maximizar
+ventana_principal.resizable(0,0)
+
+# Color de fondo ventana principal
+ventana_principal.config(bg="red")
+
+# Icono de la ventana 
+# ventana_principal.iconbitmap("suma.ico"/suma.png)
+
+# ------------------------------
+# Frame entrada datos
+# ------------------------------
+frame_entrada = tk.Frame(ventana_principal)
+frame_entrada.config(bg="white", width=480, height=240)
+frame_entrada.pack(fill=tk.BOTH)
+frame_entrada.place(x=10, y=10)
+
+# Etiqueta para el titulo
+titulo = tk.Label(frame_entrada,text="Suma de enteros")
+titulo.config(bg="black", fg="yellow", font=("Calibri", 15))
+titulo.place(x=150, y=10)
+
+# Imagen
+# logo_uis = tk.PhotoImage(file="uis.png")
+# Label_logo = tk.Label(frame_entrada, image = logo_uis)
+# Label_logo.place(x=10, y=40)
+
+# Etiqueta X
+Label_x = tk.Label(frame_entrada, text= "X =")
+Label_x.config(fg="green", font=("Calibri", 15))
+Label_x.place(x=250, y=45)
+
+# Caja de texto para X
+entry_x = tk.Entry(frame_entrada)
+entry_x.config(font=("Calibri", 13))
+entry_x.focus_set()
+entry_x.place(x=290, y=50)
+
+# Etiqueta Y
+Label_y = tk.Label(frame_entrada, text= "Y =")
+Label_y.config(fg="green", font=("Calibri", 15))
+Label_y.place(x=250, y=95)
+
+# Caja de texto para Y
+entry_y = tk.Entry(frame_entrada)
+entry_y.config(font=("Calibri", 13))
+entry_y.focus_set()
+entry_y.place(x=290, y=100)
+
+
+# ------------------------------
+# Frame operaciones
+# ------------------------------
+frame_operaciones = tk.Frame(ventana_principal)
+frame_operaciones.config(bg="white", width=480, height=120)
+frame_operaciones.pack(fill=tk.BOTH)
+frame_operaciones.place(x=10, y=255)
+
+# Boton calcular
+boton_calcular = tk.Button(frame_operaciones, text="Calcular", command=calcular)
+boton_calcular.place(x=10, y=60)
+
+# Boton borrar
+boton_borrar = tk.Button(frame_operaciones, text="Borrar", command= borrar)
+boton_borrar.place(x=160, y=60)
+
+# Boton salir
+boton_salir = tk.Button(frame_operaciones, text="Salir", command= salir)
+boton_salir.place(x=320, y=60)
+
+
+
+# ------------------------------
+# Frame resultados
+# ------------------------------
+frame_resultados = tk.Frame(ventana_principal)
+frame_resultados.config(bg="pink", width=480, height=110)
+frame_resultados.pack(fill=tk.BOTH)
+frame_resultados.place(x=10, y=380)
+
+# Desplegar ventana principal, y queda a la espera de los eventos del usuario
+ventana_principal.mainloop()
